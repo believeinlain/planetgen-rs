fbb21c9a7e7e5bb2b4340f0bba64a1e2f43e7c08
Stephanie Aelmore <steph.aelmore@gmail.com>
2022-08-13 00:05:24 -1000

    fixed index buffer

--- planetgen/src/geometry.rs
+++ planetgen/src/geometry.rs
@@ -51,39 +51,31 @@ impl Vert {
 
 #[derive(Debug, Default)]
 struct Edge {
-    endpoint: (Rc<Vert>, Rc<Vert>),
+    endpoint: [Rc<Vert>; 2],
 }
 impl Edge {
-    fn new(verts: &[Rc<Vert>], endpoints: (usize, usize)) -> Self {
+    fn new(verts: &[Rc<Vert>], endpoints: [usize; 2]) -> Self {
         Edge {
-            endpoint: (verts[endpoints.0].clone(), verts[endpoints.1].clone()),
+            endpoint: endpoints.map(|i| verts[i].clone()),
         }
     }
 }
 
 #[derive(Debug, Default)]
 struct Face {
-    corner: (Rc<Vert>, Rc<Vert>, Rc<Vert>),
-    edge: (Rc<Edge>, Rc<Edge>, Rc<Edge>),
+    corner: [Rc<Vert>; 3],
+    edge: [Rc<Edge>; 3],
 }
 impl Face {
     fn new(
         verts: &[Rc<Vert>],
         edges: &[Rc<Edge>],
-        corners: (usize, usize, usize),
-        sides: (usize, usize, usize),
+        corners: [usize; 3],
+        sides: [usize; 3],
     ) -> Self {
         Face {
-            corner: (
-                verts[corners.0].clone(),
-                verts[corners.1].clone(),
-                verts[corners.2].clone(),
-            ),
-            edge: (
-                edges[sides.0].clone(),
-                edges[sides.1].clone(),
-                edges[sides.2].clone(),
-            ),
+            corner: corners.map(|i| verts[i].clone()),
+            edge: sides.map(|i| edges[i].clone()),
         }
     }
 }
@@ -142,40 +134,40 @@ impl Icosahedron {
         let edges = array::from_fn(|i| {
             Rc::new(match i {
                 // add top edges connecting to top vertex
-                0 => Edge::new(&verts, (0, 1)),
-                1 => Edge::new(&verts, (0, 1)),
-                2 => Edge::new(&verts, (0, 1)),
-                3 => Edge::new(&verts, (0, 1)),
-                4 => Edge::new(&verts, (0, 1)),
+                0 => Edge::new(&verts, [0, 1]),
+                1 => Edge::new(&verts, [0, 2]),
+                2 => Edge::new(&verts, [0, 3]),
+                3 => Edge::new(&verts, [0, 4]),
+                4 => Edge::new(&verts, [0, 5]),
                 // add top ring of edges
-                5 => Edge::new(&verts, (0, 1)),
-                6 => Edge::new(&verts, (0, 1)),
-                7 => Edge::new(&verts, (0, 1)),
-                8 => Edge::new(&verts, (0, 1)),
-                9 => Edge::new(&verts, (0, 1)),
+                5 => Edge::new(&verts, [1, 2]),
+                6 => Edge::new(&verts, [2, 3]),
+                7 => Edge::new(&verts, [3, 4]),
+                8 => Edge::new(&verts, [4, 5]),
+                9 => Edge::new(&verts, [5, 1]),
                 // add middle zigzag edges
-                10 => Edge::new(&verts, (0, 1)),
-                11 => Edge::new(&verts, (0, 1)),
-                12 => Edge::new(&verts, (0, 1)),
-                13 => Edge::new(&verts, (0, 1)),
-                14 => Edge::new(&verts, (0, 1)),
-                15 => Edge::new(&verts, (0, 1)),
-                16 => Edge::new(&verts, (0, 1)),
-                17 => Edge::new(&verts, (0, 1)),
-                18 => Edge::new(&verts, (0, 1)),
-                19 => Edge::new(&verts, (0, 1)),
+                10 => Edge::new(&verts, [6, 1]),
+                11 => Edge::new(&verts, [1, 7]),
+                12 => Edge::new(&verts, [7, 2]),
+                13 => Edge::new(&verts, [2, 8]),
+                14 => Edge::new(&verts, [8, 3]),
+                15 => Edge::new(&verts, [3, 9]),
+                16 => Edge::new(&verts, [9, 4]),
+                17 => Edge::new(&verts, [4, 10]),
+                18 => Edge::new(&verts, [10, 5]),
+                19 => Edge::new(&verts, [5, 6]),
                 // add bottom ring of edges
-                20 => Edge::new(&verts, (0, 1)),
-                21 => Edge::new(&verts, (0, 1)),
-                22 => Edge::new(&verts, (0, 1)),
-                23 => Edge::new(&verts, (0, 1)),
-                24 => Edge::new(&verts, (0, 1)),
+                20 => Edge::new(&verts, [6, 7]),
+                21 => Edge::new(&verts, [7, 8]),
+                22 => Edge::new(&verts, [8, 9]),
+                23 => Edge::new(&verts, [9, 10]),
+                24 => Edge::new(&verts, [10, 6]),
                 // add bottom edges connecting to bottom vertex
-                25 => Edge::new(&verts, (0, 1)),
-                26 => Edge::new(&verts, (0, 1)),
-                27 => Edge::new(&verts, (0, 1)),
-                28 => Edge::new(&verts, (0, 1)),
-                29 => Edge::new(&verts, (0, 1)),
+                25 => Edge::new(&verts, [11, 6]),
+                26 => Edge::new(&verts, [11, 7]),
+                27 => Edge::new(&verts, [11, 8]),
+                28 => Edge::new(&verts, [11, 9]),
+                29 => Edge::new(&verts, [11, 10]),
                 _ => panic!("Invalid number of edges for Icosahedron"),
             })
         });
@@ -184,28 +176,28 @@ impl Icosahedron {
         let faces = array::from_fn(|i| {
             Rc::new(match i {
                 // top faces
-                0 => Face::new(&verts, &edges, (2, 1, 0), (0, 1, 5)),
-                1 => Face::new(&verts, &edges, (3, 2, 0), (1, 2, 6)),
-                2 => Face::new(&verts, &edges, (4, 3, 0), (2, 3, 7)),
-                3 => Face::new(&verts, &edges, (5, 4, 0), (3, 4, 8)),
-                4 => Face::new(&verts, &edges, (1, 5, 0), (4, 0, 9)),
+                0 => Face::new(&verts, &edges, [2, 1, 0], [0, 1, 5]),
+                1 => Face::new(&verts, &edges, [3, 2, 0], [1, 2, 6]),
+                2 => Face::new(&verts, &edges, [4, 3, 0], [2, 3, 7]),
+                3 => Face::new(&verts, &edges, [5, 4, 0], [3, 4, 8]),
+                4 => Face::new(&verts, &edges, [1, 5, 0], [4, 0, 9]),
                 // ring faces
-                5 => Face::new(&verts, &edges, (7, 6, 1), (20, 10, 11)),
-                6 => Face::new(&verts, &edges, (7, 1, 2), (5, 11, 12)),
-                7 => Face::new(&verts, &edges, (8, 7, 2), (21, 12, 13)),
-                8 => Face::new(&verts, &edges, (8, 2, 3), (6, 13, 14)),
-                9 => Face::new(&verts, &edges, (9, 8, 3), (22, 14, 15)),
-                10 => Face::new(&verts, &edges, (9, 3, 4), (7, 15, 16)),
-                11 => Face::new(&verts, &edges, (10, 9, 4), (23, 16, 17)),
-                12 => Face::new(&verts, &edges, (10, 4, 5), (8, 17, 18)),
-                13 => Face::new(&verts, &edges, (6, 10, 5), (24, 18, 19)),
-                14 => Face::new(&verts, &edges, (6, 5, 1), (9, 19, 10)),
+                5 => Face::new(&verts, &edges, [7, 6, 1], [20, 10, 11]),
+                6 => Face::new(&verts, &edges, [7, 1, 2], [5, 11, 12]),
+                7 => Face::new(&verts, &edges, [8, 7, 2], [21, 12, 13]),
+                8 => Face::new(&verts, &edges, [8, 2, 3], [6, 13, 14]),
+                9 => Face::new(&verts, &edges, [9, 8, 3], [22, 14, 15]),
+                10 => Face::new(&verts, &edges, [9, 3, 4], [7, 15, 16]),
+                11 => Face::new(&verts, &edges, [10, 9, 4], [23, 16, 17]),
+                12 => Face::new(&verts, &edges, [10, 4, 5], [8, 17, 18]),
+                13 => Face::new(&verts, &edges, [6, 10, 5], [24, 18, 19]),
+                14 => Face::new(&verts, &edges, [6, 5, 1], [9, 19, 10]),
                 // bottom faces
-                15 => Face::new(&verts, &edges, (6, 7, 11), (25, 26, 20)),
-                16 => Face::new(&verts, &edges, (7, 8, 11), (26, 27, 21)),
-                17 => Face::new(&verts, &edges, (8, 9, 11), (27, 28, 22)),
-                18 => Face::new(&verts, &edges, (9, 10, 11), (28, 29, 23)),
-                19 => Face::new(&verts, &edges, (10, 6, 11), (29, 25, 24)),
+                15 => Face::new(&verts, &edges, [6, 7, 11], [25, 26, 20]),
+                16 => Face::new(&verts, &edges, [7, 8, 11], [26, 27, 21]),
+                17 => Face::new(&verts, &edges, [8, 9, 11], [27, 28, 22]),
+                18 => Face::new(&verts, &edges, [9, 10, 11], [28, 29, 23]),
+                19 => Face::new(&verts, &edges, [10, 6, 11], [29, 25, 24]),
                 _ => panic!("Invalid number of faces for Icosahedron"),
             })
         });
@@ -224,12 +216,8 @@ impl Icosahedron {
         }))
     }
     pub fn get_index_buffer(&self) -> Vec<graphics::Index> {
-        Vec::from_iter(self.faces.iter().flat_map(|f| {
-            [
-                f.corner.0.get_index() as graphics::Index,
-                f.corner.1.get_index() as graphics::Index,
-                f.corner.2.get_index() as graphics::Index,
-            ]
-        }))
+        Vec::from_iter(self.faces.iter().flat_map(|f|
+            f.corner.iter().map(|i| i.get_index() as graphics::Index)
+        ))
     }
 }
