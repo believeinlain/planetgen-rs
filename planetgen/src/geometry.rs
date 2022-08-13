ee4012feb25a478946b97c127f11a57df4bc3672
Stephanie Aelmore <steph.aelmore@gmail.com>
2022-08-12 22:44:21 -1000

    finally figured out references

--- planetgen/src/geometry.rs
+++ planetgen/src/geometry.rs
@@ -1,105 +1,153 @@
-#[derive(Debug, Default, Clone, Copy)]
+use std::{array, cell::RefCell, rc::Rc};
+
+// wrapping coordinates in a RefCell allows vertices to move
+type MutScalar = RefCell<f32>;
+
+type VertIndex = usize;
+type EdgeIndex = usize;
+type FaceIndex = usize;
+
+#[derive(Debug, Default, Clone)]
 struct Vert {
-    x: f32,
-    y: f32,
-    z: f32,
+    x: MutScalar,
+    y: MutScalar,
+    z: MutScalar,
+}
+impl Vert {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Vert {
+            x: MutScalar::new(x),
+            y: MutScalar::new(y),
+            z: MutScalar::new(z),
+        }
+    }
+    pub fn set(&self, x: f32, y: f32, z: f32) {
+        self.x.replace(x);
+        self.y.replace(y);
+        self.z.replace(z);
+    }
+    pub fn set_x(&self, x: f32) {
+        self.x.replace(x);
+    }
+    pub fn set_y(&self, y: f32) {
+        self.y.replace(y);
+    }
+    pub fn set_z(&self, z: f32) {
+        self.z.replace(z);
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Edge {
-    endpoint: (usize, usize),
+    endpoint: (Rc<Vert>, Rc<Vert>),
+}
+impl Edge {
+    pub fn new(vert_array: &[Rc<Vert>], endpoints: (VertIndex, VertIndex)) -> Self {
+        Edge {
+            endpoint: (
+                // cloning the Rc simply creates another reference
+                // to the same data
+                vert_array[endpoints.0].clone(),
+                vert_array[endpoints.1].clone(),
+            ),
+        }
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Face {
-    vertex: (usize, usize, usize),
+    corner: (VertIndex, VertIndex, VertIndex),
+    edge: (EdgeIndex, EdgeIndex, EdgeIndex),
 }
 
 #[derive(Debug)]
 struct Icosahedron {
     radius: f32,
-    verts: [Vert; 12],
-    edges: [Edge; 30],
+    verts: [Rc<Vert>; 12],
+    edges: [Rc<Edge>; 30],
 }
 impl Icosahedron {
     fn new(radius: f32) -> Self {
-        // initialize all verts at 0, 0, 0
-        let mut verts = [Vert::default(); 12];
+        // create array of vertices
+        let verts = array::from_fn(|i| {
+            Rc::new({
+                // calculate latitude and longitude angles
+                let lat_angle = f32::atan(0.5);
+                let long_angle = f32::to_radians(36.0);
 
-        // top
-        verts[0].z = radius;
-        // bottom
-        verts[11].z = -radius;
+                // top ring is the opposite side of a triangle
+                // with hypotenuse radius and angle latitude_angle
+                let top_ring_height = radius * lat_angle.sin();
+                let top_ring_radius = radius * lat_angle.cos();
 
-        let lat_angle = f32::atan(0.5);
-        let long_angle = f32::to_radians(36.0);
+                match i {
+                    // top
+                    0 => Vert::new(0.0, 0.0, radius),
+                    // bottom
+                    11 => Vert::new(0.0, 0.0, -radius),
+                    // top ring [1..=5]
+                    1..=5 => Vert::new(
+                        top_ring_radius * ((i - 1) as f32 * 2.0 * long_angle).cos(),
+                        top_ring_radius * ((i - 1) as f32 * 2.0 * long_angle).sin(),
+                        top_ring_height,
+                    ),
+                    // bottom ring [6..=10]
+                    6..=10 => Vert::new(
+                        top_ring_radius * (((i - 6) as f32 * 2.0 - 1.0) * long_angle).cos(),
+                        top_ring_radius * (((i - 6) as f32 * 2.0 - 1.0) * long_angle).sin(),
+                        -top_ring_height,
+                    ),
+                    _ => panic!("Invalid number of vertices for Icosahedron"),
+                }
+            })
+        });
 
-        // top ring is the opposite side of a triangle with hypotenuse radius and angle latitude_angle
-        let top_ring_height = radius * lat_angle.sin();
-        let top_ring_radius = radius * lat_angle.cos();
-
-        for i in 1..5 {
-            // top ring [1..5]
-            verts[1 + i] = Vert {
-                x: top_ring_radius * f32::cos(i as f32 * 2.0 * long_angle),
-                y: top_ring_radius * f32::sin(i as f32 * 2.0 * long_angle),
-                z: top_ring_height,
-            };
-            // bottom ring [6..10]
-            verts[6 + i] = Vert {
-                x: top_ring_radius * f32::cos((i as f32 * 2.0 - 1.0) * long_angle),
-                y: top_ring_radius * f32::sin((i as f32 * 2.0 - 1.0) * long_angle),
-                z: -top_ring_height,
-            }
-        }
-
-        // initialize edge array
-        let mut edges = Vec::<Edge>::with_capacity(30);
-
-        // add top edges connecting to top vertex
-        edges.push(Edge { endpoint: (0, 1) }); // 0
-        edges.push(Edge { endpoint: (0, 2) }); // 1
-        edges.push(Edge { endpoint: (0, 3) }); // 2
-        edges.push(Edge { endpoint: (0, 4) }); // 3
-        edges.push(Edge { endpoint: (0, 5) }); // 4
-
-        // add top ring of edges
-        edges.push(Edge { endpoint: (1, 2) }); // 5
-        edges.push(Edge { endpoint: (2, 3) }); // 6
-        edges.push(Edge { endpoint: (3, 4) }); // 7
-        edges.push(Edge { endpoint: (4, 5) }); // 8
-        edges.push(Edge { endpoint: (5, 1) }); // 9
-
-        // add middle zigzag edges
-        edges.push(Edge { endpoint: (6, 1) }); // 10
-        edges.push(Edge { endpoint: (1, 7) }); // 11
-        edges.push(Edge { endpoint: (7, 2) }); // 12
-        edges.push(Edge { endpoint: (2, 8) }); // 13
-        edges.push(Edge { endpoint: (8, 3) }); // 14
-        edges.push(Edge { endpoint: (3, 9) }); // 15
-        edges.push(Edge { endpoint: (9, 4) }); // 16
-        edges.push(Edge { endpoint: (4, 10) }); // 17
-        edges.push(Edge { endpoint: (10, 5) }); // 18
-        edges.push(Edge { endpoint: (5, 6) }); // 19
-
-        // add bottom ring of edges
-        edges.push(Edge { endpoint: (6, 7) }); // 20
-        edges.push(Edge { endpoint: (7, 8) }); // 21
-        edges.push(Edge { endpoint: (8, 9) }); // 22
-        edges.push(Edge { endpoint: (9, 10) }); // 23
-        edges.push(Edge { endpoint: (10, 6) }); // 24
-
-        // add bottom edges connecting to bottom vertex
-        edges.push(Edge { endpoint: (11, 6) }); // 25
-        edges.push(Edge { endpoint: (11, 7) }); // 26
-        edges.push(Edge { endpoint: (11, 8) }); // 27
-        edges.push(Edge { endpoint: (11, 9) }); // 28
-        edges.push(Edge { endpoint: (11, 10) }); // 29
+        // create array of edges
+        let edges = array::from_fn(|i| {
+            Rc::new(match i {
+                // add top edges connecting to top vertex
+                0 => Edge::new(&verts, (0, 1)),
+                1 => Edge::new(&verts, (0, 1)),
+                2 => Edge::new(&verts, (0, 1)),
+                3 => Edge::new(&verts, (0, 1)),
+                4 => Edge::new(&verts, (0, 1)),
+                // add top ring of edges
+                5 => Edge::new(&verts, (0, 1)),
+                6 => Edge::new(&verts, (0, 1)),
+                7 => Edge::new(&verts, (0, 1)),
+                8 => Edge::new(&verts, (0, 1)),
+                9 => Edge::new(&verts, (0, 1)),
+                // add middle zigzag edges
+                10 => Edge::new(&verts, (0, 1)),
+                11 => Edge::new(&verts, (0, 1)),
+                12 => Edge::new(&verts, (0, 1)),
+                13 => Edge::new(&verts, (0, 1)),
+                14 => Edge::new(&verts, (0, 1)),
+                15 => Edge::new(&verts, (0, 1)),
+                16 => Edge::new(&verts, (0, 1)),
+                17 => Edge::new(&verts, (0, 1)),
+                18 => Edge::new(&verts, (0, 1)),
+                19 => Edge::new(&verts, (0, 1)),
+                // add bottom ring of edges
+                20 => Edge::new(&verts, (0, 1)),
+                21 => Edge::new(&verts, (0, 1)),
+                22 => Edge::new(&verts, (0, 1)),
+                23 => Edge::new(&verts, (0, 1)),
+                24 => Edge::new(&verts, (0, 1)),
+                // add bottom edges connecting to bottom vertex
+                25 => Edge::new(&verts, (0, 1)),
+                26 => Edge::new(&verts, (0, 1)),
+                27 => Edge::new(&verts, (0, 1)),
+                28 => Edge::new(&verts, (0, 1)),
+                29 => Edge::new(&verts, (0, 1)),
+                _ => panic!("Invalid number of edges for Icosahedron"),
+            })
+        });
 
         Icosahedron {
             radius,
-            verts: verts,
-            edges: edges.try_into().unwrap(),
+            verts,
+            edges,
         }
     }
 }
--- planetgen/src/main.rs
+++ planetgen/src/main.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 mod geometry;
 pub mod window;
 
