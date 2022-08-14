6770c23e0a3d4eab7d029ec493117aaa8e0e279d
Stephanie Aelmore <steph.aelmore@gmail.com>
2022-08-13 14:33:39 -1000

    added vertex colors

--- planetgen/src/graphics/mod.rs
+++ planetgen/src/graphics/mod.rs
@@ -28,6 +28,11 @@ impl Vertex {
                     shader_location: 1,
                     format: wgpu::VertexFormat::Float32x2,
                 },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 5]>() as wgpu::BufferAddress,
+                    shader_location: 2,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
             ],
         }
     }
@@ -39,27 +44,27 @@ const VERTICES: &[Vertex] = &[
     Vertex {
         position: [-0.0868241, 0.49240386, 0.0],
         tex_coords: [0.4131759, 0.00759614],
-        color: [1.0; 3],
+        color: [1.0, 0.0, 0.5],
     }, // A
     Vertex {
         position: [-0.49513406, 0.06958647, 0.0],
         tex_coords: [0.0048659444, 0.43041354],
-        color: [1.0; 3],
+        color: [0.8, 0.2, 0.5],
     }, // B
     Vertex {
         position: [-0.21918549, -0.44939706, 0.0],
         tex_coords: [0.28081453, 0.949397],
-        color: [1.0; 3],
+        color: [0.6, 0.4, 0.5],
     }, // C
     Vertex {
         position: [0.35966998, -0.3473291, 0.0],
         tex_coords: [0.85967, 0.84732914],
-        color: [1.0; 3],
+        color: [0.4, 0.6, 0.5],
     }, // D
     Vertex {
         position: [0.44147372, 0.2347359, 0.0],
         tex_coords: [0.9414737, 0.2652641],
-        color: [1.0; 3],
+        color: [0.2, 0.8, 0.5],
     }, // E
 ];
 
--- planetgen/src/graphics/shader.wgsl
+++ planetgen/src/graphics/shader.wgsl
@@ -11,11 +11,13 @@ var<uniform> transform: mat4x4<f32>;
 struct VertexInput {
     @location(0) position: vec3<f32>,
     @location(1) tex_coords: vec2<f32>,
+    @location(2) color: vec3<f32>,
 }
 
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
     @location(0) tex_coords: vec2<f32>,
+    @location(1) vert_color: vec4<f32>,
 }
 
 @vertex
@@ -24,6 +26,7 @@ fn vs_main(
 ) -> VertexOutput {
     var out: VertexOutput;
     out.tex_coords = model.tex_coords;
+    out.vert_color = vec4<f32>(model.color, 1.0);
     out.clip_position = camera.view_proj * transform * vec4<f32>(model.position, 1.0);
     return out;
 }
@@ -36,5 +39,5 @@ var s_diffuse: sampler;
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return textureSample(t_diffuse, s_diffuse, in.tex_coords);
+    return in.vert_color + textureSample(t_diffuse, s_diffuse, in.tex_coords);
 }
